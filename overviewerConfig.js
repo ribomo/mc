097f89c0f421841fa2798f194904e0ab37ea902a
Ribo Mo <ribomo42@gmail.com>
2017-07-15 17:32:49 +0800

    init

---
+++ overviewerConfig.js
@@ -0,0 +1,65 @@
+var overviewerConfig = {
+    "worlds": [
+        "world"
+    ], 
+    "tilesets": [
+        {
+            "spawn": [
+                317, 
+                64, 
+                512
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1499875276, 
+            "name": "Overviewer Render (smooth_lighting)", 
+            "poititle": "Markers", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 8, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "world", 
+            "maxZoom": 8, 
+            "path": "world-smooth_lighting", 
+            "showlocationmarker": true
+        }
+    ], 
+    "CONST": {
+        "mapDivId": "mcmap", 
+        "UPPERLEFT": 0, 
+        "tileSize": 384, 
+        "UPPERRIGHT": 1, 
+        "image": {
+            "defaultMarker": "signpost.png", 
+            "queryMarker": "https://google-maps-icons.googlecode.com/files/regroup.png", 
+            "signMarker": "signpost_icon.png", 
+            "spawnMarker": "https://google-maps-icons.googlecode.com/files/home.png", 
+            "bedMarker": "bed.png"
+        }, 
+        "LOWERRIGHT": 2, 
+        "LOWERLEFT": 3, 
+        "regionStrokeWeight": 2
+    }, 
+    "map": {
+        "debug": true, 
+        "north_direction": "lower-left", 
+        "controls": {
+            "spawn": true, 
+            "mapType": true, 
+            "compass": true, 
+            "coordsBox": true, 
+            "overlays": true, 
+            "searchBox": true, 
+            "zoom": true, 
+            "pan": true
+        }, 
+        "center": [
+            -314, 
+            67, 
+            94
+        ], 
+        "cacheTag": "1500109285"
+    }
+};
